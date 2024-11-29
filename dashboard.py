60f17bcbf7963ace9ac692460314e592c9ab9208
Your Name <you@example.com>
2024-11-29 16:15:19 +0100

    added raw path

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,8 @@ import pandas as pd
 st.title("Kinova Dashboard")
 
 # path to CSV file
-csv_file_path = '/home/student/Kinova Temperature/DataLogger/0_KinovaLog.csv'  
+# csv_file_path = '/home/student/Kinova Temperature/DataLogger/0_KinovaLog.csv'  #local path
+csv_file_path = 'https://raw.githubusercontent.com/TKL01/Kinova-Dashboard/refs/heads/main/0_KinovaLog.csv'    #raw github repo path for streamlit cloud  
 
 # Read CSV
 df = pd.read_csv(csv_file_path)
